73d7cf0d8c725cb7184089e002a421506e854d81
Jelle Monen <jelle.monen@hotmail.com>
2019-07-11 15:01:36 +0200

    added action and reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import word from './word'
 import game from './game'
 import games from './games'
+import wheel from './wheel'
 
 export default combineReducers({
   word,
   game,
-  games
+  games,
+  wheel
 })
