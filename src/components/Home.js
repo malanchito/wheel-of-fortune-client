5c888bb33dc6dcbf1670b89a2ec2505cfb0796e4
mnezmah <mario.nezmah@gmail.com>
2019-07-12 14:43:26 +0200

    PlayerUpdate working

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import Login from './Login'
 import Player from './Player'
 import './Home.css'
 import { setGames } from '../actions/games'
+import { updatePlayer } from '../actions/player'
 
 class Home extends React.Component {
   source = new EventSource('https://wheel-of-fortune-server.herokuapp.com/stream')
@@ -12,18 +13,38 @@ class Home extends React.Component {
   componentDidMount() {
     this.source.onmessage = this.props.setGames
   }
+  onSubmit = (event) => {
+    event.preventDefault()
+    this.props.updatePlayer(this.props.player.id, event.target.value)
+  }
 
   render() {
+    // const gameId = this.props.game.id
     return (
       this.props.games.length ?
         <div className="home">
+          <Player />
+          <p>{!this.props.player.name ? 'Please enter a player name' : `Welcome ${this.props.player.name}, choose your category`}</p>
           <ul>
             {Object
               .values(this.props.games[0])
-              .map(game => <li key={game.id}>{game.id}</li>)}
+              .map(game =>
+                <div>
+                  <button
+                    type="submit"
+                    onClick={this.onSubmit}
+                    value={game.id}
+                  >
+                    Select a game {game.id}
+                  </button>
+                </div>
+                //  <Link to={`/game/${game.id}/${this.props.player.name}`}> <li key={game.id}>{game.id} category name</li>  
+                //   </Link> 
+              )}
           </ul>
+          <br />
           <Login />
-          <Player />
+
           <h1>Lobby</h1>
           <p>Click the links below to play the game and learn how the game works</p>
           <Link to="/tutorial">Learn to play</Link>
@@ -35,9 +56,10 @@ class Home extends React.Component {
 
 const mapStateToProps = (state) => {
   return {
-    games: state.games
+    games: state.games,
+    player: state.player
   }
 }
 
-const mapDispatchToProps = { setGames }
+const mapDispatchToProps = { setGames, updatePlayer }
 export default connect(mapStateToProps, mapDispatchToProps)(Home)
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,7 +1,6 @@
 import { USER_CREATED } from '../actions/user'
 
 const user = (state = {}, action = {}) => {
-  console.log('CAN I SEE MY ACTION?')
   switch (action.type) {
     case USER_CREATED:
       return {
